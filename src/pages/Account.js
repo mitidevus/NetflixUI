bd99512a291aa333df5ffa291c2d10b3d66ea3e8
Đỗ Minh Trí <90955782+mitidevus@users.noreply.github.com>
2022-09-27 12:51:45 +0700

    Done

---
+++ src/pages/Account.js
@@ -0,0 +1,23 @@
+import React from "react";
+import SavedShows from "../components/SavedShows";
+
+function Account() {
+  return (
+    <>
+      <div className="w-full text-white">
+        <img
+          className="w-full h-[400px] object-cover"
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/45082c39-e6d5-4b02-8867-e38fe32ed3af/e5b7469b-7098-4e7b-9830-6075c65a9d97/VN-en-20220919-popsignuptwoweeks-perspective_alpha_website_large.jpg"
+          alt="/"
+        ></img>
+        <div className='bg-black/60 fixed top-0 left-0 w-full h-[550px]'></div>
+        <div className='absolute top-[20%] p-4 md:p-8'>
+          <h1 className='text-3xl md:text-5xl font-bold'>My Shows</h1>
+        </div>
+      </div>
+      <SavedShows />
+    </>
+  );
+}
+
+export default Account;
